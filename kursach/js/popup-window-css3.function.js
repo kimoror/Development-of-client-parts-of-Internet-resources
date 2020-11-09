0ce9cbe0f98f1ba961c539b0387a5bb63c1fb135
kimoror <kimoror@mail.ru>
2020-11-09 20:35:50 +0300

    add popup-window

--- kursach/js/popup-window-css3.function.js
+++ kursach/js/popup-window-css3.function.js
@@ -3,13 +3,14 @@
 	var overlay	= document.querySelector('.overlay'),
 		mOpen	= document.querySelectorAll('[data-modal]'),
 		mClose	= document.querySelectorAll('[data-close]'),
-		mStatus	= false;
+		mStatus	= false;// флаг всплывающего окна: false - окно закрыто, true - открыто
 
-	if (mOpen.length == 0) return;
+	if (mOpen.length == 0) //если нет всплывающих окон, то завершае работу скрипта
+		return;
 
 	[].forEach.call(mOpen, function(el) {
 		el.addEventListener('click', function(e) {
-			var modalId	= el.getAttribute('data-modal'),
+			var modalId	= el.getAttribute('data-modal'),//возвращает значение указанного атрибута элемента.
 				modal	= document.getElementById(modalId);
 
 			modalShow(modal);
@@ -20,18 +21,14 @@
 		el.addEventListener('click', modalClose);
 	});
 
-	document.addEventListener('keydown', modalClose);
+	document.addEventListener('keydown', modalClose);//Чтобы можно было закрыть по нажатии esc
 
 	function modalShow(modal) {
 		overlay.classList.remove('fadeOut');
 		overlay.classList.add('fadeIn');
-		if (typeAnimate == 'fade') {
-			modal.classList.remove('fadeOut');
-			modal.classList.add('fadeIn');
-		} else if (typeAnimate == 'slide') {
-			modal.classList.remove('slideOutUp');
-			modal.classList.add('slideInDown');
-		}
+		modal.classList.remove('fadeOut');
+		modal.classList.add('fadeIn');
+
 		mStatus = true;
 	}
 
@@ -40,13 +37,8 @@
 			var modals = document.querySelectorAll('.dlg-modal');
 
 			[].forEach.call(modals, function(modal) {
-				if (typeAnimate == 'fade') {
-					modal.classList.remove('fadeIn');
-					modal.classList.add('fadeOut');
-				} else if (typeAnimate == 'slide') {
-					modal.classList.remove('slideInDown');
-					modal.classList.add('slideOutUp');
-				}
+				modal.classList.remove('fadeIn');
+				modal.classList.add('fadeOut');
 			});
 
 			overlay.classList.remove('fadeIn');
